189a76e02432bc396aef23a43af7dd4ea87a8f3e
keshkrish <keshavakrishna2001@gmail.com>
2024-04-25 23:18:29 +0530

    Task 2 completed

---
+++ task2.py
@@ -0,0 +1,54 @@
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+import keras
+from keras.preprocessing import image
+import numpy as np
+import sys
+from PIL import Image
+import io
+from imageio import v3 as iio
+from fastapi import Response
+
+
+app = FastAPI()
+
+# Function to Load the model
+def load_model(path: str):
+    return keras.models.load_model(path) 
+# Function to format the image (not used in task1)
+def format_image(org_img):
+    img_reshape=org_img.resize((28,28)) # Reshape the image to 28x28
+    img_gray=img_reshape.convert('L') # Convert image to gray scale
+    return img_gray
+
+# Function to Predict digit
+def predict_digit(model, data_point):
+    prediction = model.predict(data_point)
+    digit = np.argmax(prediction) # The predicted digit is the argmax of the output of the model
+    return str(digit)
+
+# API endpoint
+@app.post('/predict')
+async def predict(file: UploadFile = File(...)):
+    model_path = sys.argv[1]  # Get model path from command line argument
+    loaded_model = load_model(model_path) # Load the model
+    if file.content_type.startswith('image'): # Check if the input file is a image file
+        image = await file.read() # Read the input file
+        
+    else:
+        return {"error": "Uploaded file is not an image."}
+    
+    pil_image = Image.open(io.BytesIO(image)) # Open the image using PIL
+    formatted_img=format_image(pil_image) # Format the image (not done in task 1)
+    numpy_image = np.array(formatted_img) # Convert PIL image to NumPy array
+    numpy_image=numpy_image/255.0 # Convert the values to between 0 and 1
+    data_point=numpy_image.reshape(1,28*28) # Reshape the image array to create a seralized array of 784 elements
+    digit = predict_digit(loaded_model, data_point) # Use the model to predict the digit
+    
+    return {"digit": digit}
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Specify the path of the model as a command line argument and no other arguments apart from that!!!")
+        sys.exit(1)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
